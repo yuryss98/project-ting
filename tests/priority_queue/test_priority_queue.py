f7a6b268ef76e995d0dd91ddde6e7d66cf04582e
Yury Stolarz <yurysscareer@gmail.com>
2023-05-08 19:02:03 -0300

    feat: add req 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,36 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    file_process_mock_1 = {
+        'qtd_linhas': 3
+    }
+    file_process_mock_2 = {
+        'qtd_linhas': 6
+    }
+    queue = PriorityQueue()
+
+    queue.enqueue(file_process_mock_1)
+    assert len(queue.high_priority) == 1
+    assert len(queue.regular_priority) == 0
+
+    queue.enqueue(file_process_mock_2)
+    assert len(queue.high_priority) == 1
+    assert len(queue.regular_priority) == 1
+
+    search1 = queue.search(0)
+    search2 = queue.search(1)
+    assert search1 == file_process_mock_1
+    assert search2 == file_process_mock_2
+
+    queue.dequeue()
+    assert len(queue.high_priority) == 0
+    assert len(queue.regular_priority) == 1
+
+    queue.dequeue()
+    assert len(queue.high_priority) == 0
+    assert len(queue.regular_priority) == 0
+
+    with pytest.raises(IndexError):
+        queue.search(0)
