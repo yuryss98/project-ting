a17044b00175d9d672949f81f45542fbe45e277d
Yury Stolarz <yurysscareer@gmail.com>
2023-05-08 18:36:07 -0300

    feat: add req 7 and 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,43 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        result_dict = {
+                    "palavra": word,
+                    "arquivo": instance.search(i)['nome_do_arquivo'],
+                    "ocorrencias": []
+                }
+
+        for j in range(len(instance.search(i)['linhas_do_arquivo'])):
+            row = instance.search(i)['linhas_do_arquivo'][j]
+            if word.lower() in row.lower():
+                result_dict['ocorrencias'].append({"linha": j + 1})
+
+        if len(result_dict['ocorrencias']):
+            result.append(result_dict)
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        result_dict = {
+                    "palavra": word,
+                    "arquivo": instance.search(i)['nome_do_arquivo'],
+                    "ocorrencias": []
+                }
+
+        for j in range(len(instance.search(i)['linhas_do_arquivo'])):
+            row = instance.search(i)['linhas_do_arquivo'][j]
+            if word.lower() in row.lower():
+                result_dict['ocorrencias'].append({
+                    "linha": j + 1,
+                    'conteudo': row
+                })
+
+        if len(result_dict['ocorrencias']):
+            result.append(result_dict)
+
+    return result
