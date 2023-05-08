40882c7ca6c90af77d57e66c058eca56f1ffbd90
Yury Stolarz <yurysscareer@gmail.com>
2023-05-08 16:14:30 -0300

    feat: add req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        search = instance.search(i)
+        if search['nome_do_arquivo'] == path_file:
+            return
+
+    text_lines = txt_importer(path_file)
+
+    file_dict = {
+        'nome_do_arquivo':  path_file,
+        'qtd_linhas': len(text_lines),
+        'linhas_do_arquivo': text_lines
+    }
+
+    instance.enqueue(file_dict)
+    print(file_dict, file=sys.stdout)
 
 
 def remove(instance):
