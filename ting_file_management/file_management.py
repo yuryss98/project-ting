a5fe7e02a144fc03aeaf50a439086d82d5f84bcb
Yury Stolarz <yurysscareer@gmail.com>
2023-05-08 15:36:36 -0300

    feat: add req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    ...
+    try:
+        if not path_file.lower().endswith(".txt"):
+            return print("Formato inválido", file=sys.stderr)
+
+        with open(path_file, "r") as file:
+            text = file.readlines()
+            return [i.replace('\n', '') for i in text]
+
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
